3ec6f300a61c4c08fe4c083961927cd9872e9c12
Romelium <author@romelium.cc>
2026-05-17 07:39:25 +0800

    refactor(extensions): use ActivityCompat for permissions
    Updates permission request methods to use ActivityCompat instead of the Activity-level requestPermissions. This ensures better compatibility and consistent behavior across different Android versions.

--- app/src/main/java/com/aravi/dot/extensions/Extensions+Activity.kt
+++ app/src/main/java/com/aravi/dot/extensions/Extensions+Activity.kt
@@ -2,15 +2,16 @@ package com.aravi.dot.extensions
 
 import android.app.Activity
 import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
 
 fun Activity.permission(permission: String) {
     if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
-        requestPermissions(arrayOf(permission), 12030)
+        ActivityCompat.requestPermissions(this, arrayOf(permission), 12030)
     }
 }
 
 fun Activity.permissions(vararg permissions: String) {
-    requestPermissions(arrayOf(*permissions), 12030)
+    ActivityCompat.requestPermissions(this, arrayOf(*permissions), 12030)
 }
